1e0a0043305f2180de23f10a62ae28bdc6468e16
hvotava <57815550+hvotava@users.noreply.github.com>
2025-08-19 09:14:12 +0200

    Force redeploy to reset WebSocket state

--- react-dashboard/backend/server.js
+++ react-dashboard/backend/server.js
@@ -424,4 +424,4 @@ const startServer = async () => {
 // setupTwilioWebSocket(server);
 
 startServer(); // Force Railway redeploy - Wed Aug  6 18:38:35 CEST 2025
-// FORCE REDEPLOY - Wed Aug  6 22:27:56 CEST 2025
+// FORCE REDEPLOY - Tue Aug 19 09:15:00 CEST 2025 - Fix WebSocket state
